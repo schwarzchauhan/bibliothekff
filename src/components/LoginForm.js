fb0e2ed8c39960d192a5f7bd5aab0144f7a7b08d
schwarzchauhan <harshchauhan0994@gmail.com>
2022-01-26 16:29:09 +0530

    first commit

---
+++ src/components/LoginForm.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+
+export default function LoginForm({Login, error}) {
+  
+const [details, setDetails] = useState({name: "", email: "", password: ""});
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+        Login(details);
+    }
+
+    return (
+     <form onSubmit={submitHandler}>
+         <div className='form-innner'>
+            <h2>LOGIN</h2>
+            {  
+                /*Error*/
+                (error != "") ? (
+                    <div className='loginError'>{error}</div>
+                ) :(
+                    ""
+                ) 
+            }
+            <div className='form-group'>
+                <label htmlFor='name'>Name: </label>
+                <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name} />
+            </div>
+            <div className='form-group'>
+                <label htmlFor='email'>Email: </label>
+                <input type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email} />
+            </div>
+            <div className='form-group'>
+                <label htmlFor='password'>Password: </label>
+                <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/>
+            </div>
+            <input type="submit" value="LOGIN" />
+            
+         </div>
+     </form>
+    )
+}
---
+++ src/components/chatbot.js
@@ -0,0 +1,9 @@
+import { React } from "react";
+
+
+
+const Chatbot =  () => {
+    return <h1>chatbot</h1>
+}
+
+export default Chatbot
