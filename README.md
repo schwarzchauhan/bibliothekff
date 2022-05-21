2031d2278f4fe42d86ba950b727d4de4010b8285
schwarzchauhan <harshchauhan0994@gmail.com>
2022-05-21 06:26:33 +0530

    TOOD form  for loop for mcq

--- README.md
+++ README.md
@@ -86,6 +86,8 @@ dev dependencies
 [sass - parent selctor](https://sass-lang.com/documentation/style-rules/parent-selector)  \
 [conditional operator in reactjs](https://reactjs.org/docs/conditional-rendering.html#inline-if-with-logical--operator)
 
+- [Bootstrap 5](https://getbootstrap.com/docs/5.0/getting-started/introduction/)  
+ [cdn](https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css)
 
 ## hosting on netlify
 > `CI=false npm run build`
--- src/components/Custom/Mcq.js
+++ src/components/Custom/Mcq.js
@@ -5,40 +5,56 @@ import React from "react";
 class Mcq extends React.Component {
     render() {
         return (
-            <div className="container">
-                <div className="question ml-sm-5 pl-sm-5 pt-2">
-                    <div className="py-2 h5"><b>Q. which option best describes your job role?</b></div>
-                    <div className="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">
-                        <label className="options">Small Business Owner or Employee
-                            <input type="radio" name="radio" />
-                            <span className="checkmark"></span>
-                        </label>
-                        <label className="options">Nonprofit Owner or Employee
-                            <input type="radio" name="radio" />
-                            <span className="checkmark"></span>
-                        </label>
-                        <label className="options">Journalist or Activist
-                            <input type="radio" name="radio" />
-                            <span className="checkmark"></span>
-                        </label>
-                        <label className="options">Other
-                            <input type="radio" name="radio" />
-                            <span className="checkmark"></span>
-                        </label>
-                    </div>
-                </div>
-                <div className="d-flex align-items-center pt-3">
-                    <div id="prev">
-                        <button className="btn btn-primary">Previous</button>
-                    </div>
-                    <div className="ml-auto mr-sm-5">
-                        <button className="btn btn-success">Next</button>
+            <div className="mcq-cont">
+                <div className="container mt-5">
+                    <div className="d-flex justify-content-center row">
+                        <div className="col-md-10 col-lg-10">
+                            <div className="border">
+                                <div className="bg-info p-3 border-bottom">
+                                    <div className="d-flex flex-row justify-content-between align-items-center">
+                                        <h4>MCQ Quiz</h4>
+                                        <span>(5 of 20)</span>
+                                    </div>
+                                </div>
+                                <div className="p-3 border-bottom">
+                                    <div className="d-flex flex-row">
+                                        <h3 className="text-danger">Q. </h3>
+                                        <h5 className="mt-1">Which of the following country has largest population?</h5>
+                                    </div>
+                                    <div className="mcq-radio">
+                                        <label>
+                                            <input type="radio" name="brazil" value="brazil" />
+                                            <span>Brazil</span>
+                                        </label>
+                                    </div>
+                                    <div className="mcq-radio">
+                                        <label>
+                                            <input type="radio" name="Germany" value="Germany" />
+                                            <span>Germany</span>
+                                        </label>
+                                    </div>
+                                    <div className="mcq-radio">
+                                        <label>
+                                            <input type="radio" name="Indonesia" value="Indonesia" />
+                                            <span>Indonesia</span>
+                                        </label>
+                                    </div>
+                                    <div className="mcq-radio">
+                                        <label>
+                                            <input type="radio" name="Russia" value="Russia" />
+                                            <span>Russia</span>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div className="d-flex flex-row justify-content-between align-items-center p-3 bg-white"><button className="btn btn-primary d-flex align-items-center btn-danger" type="button"><i className="fa fa-angle-left mt-1 mr-1"></i>&nbsp;previous</button><button className="btn btn-primary border-success align-items-center btn-success" type="button">Next<i className="fa fa-angle-right"></i></button></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         )
     }
-  }
+}
 
 
 export default Mcq
